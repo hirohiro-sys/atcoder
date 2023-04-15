8a13c23d346fa291aa678b4bb9015d741354767d
hirohiro-sys <katohiro8843@icloud.com>
2023-04-15 23:49:28 +0900

    ABC298の復習

--- ABC298/ABC298b.py
+++ ABC298/ABC298b.py
@@ -1,15 +1,15 @@
 N = int(input())
-A = [list(map(int, input().split())) for _ in range(N)]
-B = [list(map(int, input().split())) for _ in range(N)]
-
+A = [list(map(int,input().split())) for _ in range(N)]
+B = [list(map(int,input().split())) for _ in range(N)]
 for _ in range(4):
     ans = True
-    A = list(zip(*A))[::-1]        #90度反転
+    A = list(zip(*A))[::-1]
     for i in range(N):
         for j in range(N):
             if A[i][j] == 1 and B[i][j] == 0:
-                ans = False
+                ans==False
     if ans:
-        print('Yes')
+        print("Yes")
         exit()
-print('No')
+print(ans)
+

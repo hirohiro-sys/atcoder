f1294a97c83293c1a5042a85bde609d65baffb5f
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-04-28 22:12:07 +0900

    Update ABC274b.py

--- B_practice/ABC274b.py
+++ B_practice/ABC274b.py
@@ -3,6 +3,7 @@ li = [input() for i in range(h)]
 for i in range(len(li[0])):
   count = 0
   for j in range(len(li)):
-    if li[j][i]=="#":      #今回は二次元配列を列から見てるからj→iの順番で参照
+    #今回は二次元配列を列から見てるからj→iの順番で参照
+    if li[j][i]=="#":      
       count += 1
   print(count,end=" ")
